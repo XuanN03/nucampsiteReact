0e459eb8a6a6ebbfc826ff59032713484faef3ab
Xuan Nguyen <xtnguyen03@gmail.com>
2025-03-06 16:37:56 -0500

    redux campsites slice

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,4 +1,16 @@
 import { CAMPSITES } from '../../app/shared/CAMPSITES';
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    campsitesArray: CAMPSITES
+};
+
+const campsitesSlice = createSlice({
+    name: 'campsites',
+    initialState
+});
+
+export const campsitesReducer = campsitesSlice.reducer;
 
 export const selectAllCampsites = () => {
     return CAMPSITES;
@@ -15,3 +27,5 @@ export const selectCampsiteById = (id) => {
 export const selectFeaturedCampsite = () => {
     return CAMPSITES.find((campsite) => campsite.featured);
 };
+
+
